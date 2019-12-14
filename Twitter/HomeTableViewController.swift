94587d175a2b52d9f2b584b1866b2c4bb842346a
markFalcone <falconemark16@yahoo.com>
2019-12-13 21:57:01 -0800

    Implemented infinite scroll

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -18,14 +18,15 @@ class HomeTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         loadTweets()
+        twitterRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
+        tableView.refreshControl = twitterRefreshControl
     }
-    
+    // loading tweets
      @objc func loadTweets() {
+        numberOfTweets = 20
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-        let myParams = ["count": 10]
-        
-        
-        
+        let myParams = ["count": numberOfTweets]
+                
        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
 
             self.tweetArray.removeAll()
@@ -43,6 +44,34 @@ class HomeTableViewController: UITableViewController {
     }
     
     
+    func loadMoreTweets(){
+        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        numberOfTweets = numberOfTweets + 20
+        let myParams = ["count": numberOfTweets]
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
+                   
+                   self.tweetArray.removeAll()
+                   
+                   for tweet in tweets {
+                       self.tweetArray.append(tweet)
+                   }
+                   
+                   self.tableView.reloadData()
+                   
+               }, failure: { (Error) in
+                   print("Could not retrieve tweets!")
+               })
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row + 1 == tweetArray.count {
+            loadMoreTweets()
+        }
+    }
+    
+    
+    //logout
     @IBAction func onLogout(_ sender: Any) {
         TwitterAPICaller.client?.logout()
         self.dismiss(animated: true, completion: nil)
